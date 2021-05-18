f55668e5272dc091523cdb8505dc41104102fdfb
istiaksaif <istiaksaif.nsu@gmail.com>
2021-05-18 23:42:31 +0600

    Some update

--- app/src/main/java/com/example/ahar/Fragment/MapFragment.java
+++ app/src/main/java/com/example/ahar/Fragment/MapFragment.java
@@ -1,8 +1,13 @@
 package com.example.ahar.Fragment;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -26,6 +31,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 /**
  * Created by Amirul Simon on 05/05/21.
  * Updated by Istiak Saif 08/05/21.
@@ -35,6 +45,13 @@ public class MapFragment extends Fragment {
     private FusedLocationProviderClient client;
     private SupportMapFragment mapFragment;
     private int REQUEST_CODE = 111;
+    private ConnectivityManager connectivityManager;
+    private NetworkInfo networkInfo;
+    private GoogleMap map,googlemap;
+    private Geocoder geocoder;
+    private double selectedlat, selectedlng;
+    private List<Address> addressList;
+    private String string;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -67,13 +84,33 @@ public class MapFragment extends Fragment {
                     mapFragment.getMapAsync(new OnMapReadyCallback() {
                         @Override
                         public void onMapReady(GoogleMap googleMap) {
-                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            map = googleMap;
+                            googlemap = googleMap;
+                            getAddress(location.getLatitude(),location.getLongitude());
+//                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//
+//                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(string);
+//
+//                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+//
+//                            googleMap.addMarker(markerOptions).showInfoWindow();
 
-                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here");
+                            map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                                @Override
+                                public void onMapClick(LatLng latLng) {
+                                    connectionCheck();
+                                    if(networkInfo.isConnected() && networkInfo.isAvailable()){
 
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+                                        selectedlat = latLng.latitude;
+                                        selectedlng = latLng.longitude;
 
-                            googleMap.addMarker(markerOptions).showInfoWindow();
+                                        getAddress(selectedlat,selectedlng);
+
+                                    }else {
+                                        Toast.makeText(getActivity(), "Connection check", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                         }
                     });
                 }
@@ -93,6 +130,41 @@ public class MapFragment extends Fragment {
         }
     }
 
+    private void connectionCheck(){
+        connectivityManager = (ConnectivityManager) getActivity().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = connectivityManager.getActiveNetworkInfo();
+
+    }
+    private void getAddress(double mlat, double mlng){
+        geocoder = new Geocoder(getActivity(), Locale.getDefault());
+        if(mlat != 0){
+            try {
+                addressList = geocoder.getFromLocation(mlat,mlng,1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(addressList != null){
+                String maddress = addressList.get(0).getAddressLine(0);
+
+                string = maddress;
+
+                if(maddress != null){
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    LatLng latLng = new LatLng(mlat,mlng);
+                    markerOptions.position(latLng).title(string);
+                    map.addMarker(markerOptions).showInfoWindow();
+                    googlemap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+                    googlemap.addMarker(markerOptions).showInfoWindow();
+                }else {
+                    Toast.makeText(getActivity(), "Something error", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(getActivity(), "Something error", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(getActivity(), "LatLng null", Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
